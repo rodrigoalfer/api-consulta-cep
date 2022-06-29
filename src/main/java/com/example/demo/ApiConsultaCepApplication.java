c621a5bfb74d412da72f8ba63ea9bbbedf4bfeb4
Rodrigo Alberto Ferreira <107878313+rodrigoalfer@users.noreply.github.com>
2022-06-28 21:16:10 -0300

    commit de teste: adiciona uma linha que será removida depois

--- src/main/java/com/example/demo/ApiConsultaCepApplication.java
+++ src/main/java/com/example/demo/ApiConsultaCepApplication.java
@@ -8,6 +8,9 @@ public class ApiConsultaCepApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiConsultaCepApplication.class, args);
+		
+		//linha para testar o commit no Sourcetree
+		int soma = 1+1;
 	}
 
 }
